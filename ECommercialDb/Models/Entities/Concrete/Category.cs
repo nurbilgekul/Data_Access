5b6b2b8464a04e76f7083be545751b823bafaed1
nurbilgekul <nurbilgekul@gmail.com>
2021-10-13 23:27:02 +0300

    Just Begin

---
+++ ECommercialDb/Models/Entities/Concrete/Category.cs
@@ -0,0 +1,13 @@
+﻿using ECommercialDb.Models.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercialDb.Models.Entities.Concrete
+{
+    public class Category:BaseEntity
+    {
+    }
+}
---
+++ ECommercialDb/Models/Entities/Concrete/Product.cs
@@ -0,0 +1,18 @@
+﻿using ECommercialDb.Models.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommercialDb.Models.Entities.Concrete
+{
+    public class Product:BaseEntity
+    {
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+        public int SupplierId { get; set; }
+        public virtual Supplier Supplier { get; set; }
+    }
+}
